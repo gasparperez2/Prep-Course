b43cbb7e12d462e2d73ef741739c478d800fbbf1
Gaspar Perez <gasparperez171@gmail.com>
2022-03-11 20:21:51 -0300

    Tarea JSX completada

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -9,7 +9,18 @@ function deObjetoAmatriz(objeto){
       B: 2,
       C: 3
     }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
-  //Escribe tu código aquí
+  //Escribe tu código aquí:
+  var nuevoArray = []
+  var keys = Object.keys(objeto)
+  var values = Object.values(objeto)
+
+  for (i=0; i<keys.length; i++) {
+    for (j=0; j<values.length; j++) {
+      nuevoArray.push ([keys[i], values[j]])
+      i++
+    }
+  }
+  return nuevoArray;
 }
 
 
@@ -18,6 +29,16 @@ function numberOfCharacters(string) {
   //en formato par clave-valor.
   //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
   //Escribe tu código aquí
+  var obj = {}
+  for (i=0; i<string.length; i++) {
+    if (obj.hasOwnProperty(string[i])) {
+      obj[string[i]] = obj[string[i]] +1
+    }
+    else {
+      obj[string[i]] = 1
+    }
+  }
+  return obj;
 }
 
 
@@ -26,6 +47,18 @@ function capToFront(s) {
   //al principio de la palabra.
   //Ejemplo: soyHENRY -> HENRYsoy
   //Escribe tu código aquí
+  var nuevoString = ''
+  for (i=0; i<s.length; i++) {
+    if (s[i] == s[i].toUpperCase()) {
+      nuevoString= nuevoString + s[i]
+    }
+  }
+  for (j=0; j<s.length; j++) {
+    if (s[j] == s[j].toLowerCase()) {
+      nuevoString= nuevoString + s[j]
+    }
+  }
+  return nuevoString;
 }
 
 
@@ -35,6 +68,12 @@ function asAmirror(str) {
   //pero con cada una de sus palabras invertidas, como si fuera un espejo.
   //Ej: Recibe ---> "The Henry Challenge is close!" || Devuelve ---> "ehT yrneH egnellahC si !esolc"
   //Escribe tu código aquí
+  var nuevoArray = str.split(' ')
+  var rta = []
+  for (i=0; i<nuevoArray.length; i++) {
+    rta.push (nuevoArray[i].split('').reverse().join(''))
+  }
+  return rta.join (' ')
 } 
 
 
@@ -43,6 +82,13 @@ function capicua(numero){
   //La misma debe retornar: "Es capicua" si el número se número que se lee igual de 
   //izquierda a derecha que de derecha a izquierda. Caso contrario retorna "No es capicua"
   //Escribe tu código aquí
+  var stringNum = numero.toString()
+  var numeroInvertido = stringNum.split('').reverse()
+  var nuevoNum = numeroInvertido.join ('')
+  if (numero == nuevoNum) {
+    return 'Es capicua'
+  }
+  return 'No es capicua'
 }
 
 
@@ -50,6 +96,16 @@ function deleteAbc(cadena){
   //Define una función que elimine las letras "a", "b" y "c" de la cadena dada 
   //y devuelva la versión modificada o la misma cadena, en caso de contener dichas letras.
   //Escribe tu código aquí
+  var nuevoString = '';
+  for (i=0; i<cadena.length;i++) {
+    if (cadena[i] === 'a' ||cadena[i] === 'b'||cadena[i] === 'c') {
+      continue;
+    }
+    else {
+      nuevoString = nuevoString + cadena[i]
+    }
+  }
+  return nuevoString;
 }
 
 
@@ -57,6 +113,19 @@ function sortArray(arr) {
   //La función recibe una matriz de strings. Ordena la matriz en orden creciente de longitudes de cadena
   //Ej: Recibe ---> ["You", "are", "beautiful", "looking"] || Devuelve ---> [“You", "are", "looking", "beautiful"]
   //Escribe tu código aquí
+  var cambio = true;
+  while(cambio) {
+    cambio = false
+    for (i=0; i<arr.length-1; i++) {
+      if(arr[i].length >arr[i+1].length) {
+        var aux = arr[i]
+        arr[i] = arr[i+1]
+        arr[i+1] = aux;
+        cambio = true
+      }
+    }
+  }
+  return arr;
 }
 
 
@@ -65,7 +134,19 @@ function buscoInterseccion(arreglo1, arreglo2){
   //retornar un nuevo array con la intersección de ambos elementos. (Ej: [4,2,3] unión [1,3,4] = [3,4].
   //Si no tienen elementos en común, retornar un arreglo vacío.
   //Aclaración: los arreglos no necesariamente tienen la misma longitud
-  //Escribe tu código aquí  
+  //Escribe tu código aquí 
+  var union = []
+  var j = 0
+  while (j<arreglo2.length) {
+    for (i=0;i<arreglo1.length;i++) {
+      if (arreglo1[i] === arreglo2[j]) {
+        j= j+1
+        union.push(arreglo1[i]);
+      }
+    }
+    j=j+1
+  }
+  return union;
 }
 
 
